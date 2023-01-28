4ac992de79550fd3616ac112ef129a4c51208940
egarciae2021 <81602306+egarciae2021@users.noreply.github.com>
2023-01-27 21:30:56 -0500

    Create repo.js

---
+++ repo.js
@@ -0,0 +1,14 @@
+let script = document.createElement("script");
+script.src = 'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.15.5/xlsx.full.min.js';
+document.head.appendChild(script); 
+let script4 = document.createElement("script");
+script4.src = 'https://code.jquery.com/jquery-3.6.3.min.js';
+document.head.appendChild(script4); 
+
+let script2 = document.createElement("script");
+script2.src = 'https://cdnjs.cloudflare.com/ajax/libs/jqgrid/4.6.0/js/jquery.jqGrid.min.js';
+document.head.appendChild(script2); 
+
+let script3 = document.createElement("script");
+script3.src = 'https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js';
+document.head.appendChild(script3); 
