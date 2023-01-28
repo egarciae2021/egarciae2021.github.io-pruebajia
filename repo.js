ae602f665a3855832cefd482d2b057303950cecd
egarciae2021 <81602306+egarciae2021@users.noreply.github.com>
2023-01-27 21:52:59 -0500

    Update repo.js

--- repo.js
+++ repo.js
@@ -1,6 +1,7 @@
 let script = document.createElement("script");
 script.src = 'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.15.5/xlsx.full.min.js';
 document.head.appendChild(script); 
+
 let script4 = document.createElement("script");
 script4.src = 'https://code.jquery.com/jquery-3.6.3.min.js';
 document.head.appendChild(script4); 
@@ -9,6 +10,12 @@ let script2 = document.createElement("script");
 script2.src = 'https://cdnjs.cloudflare.com/ajax/libs/jqgrid/4.6.0/js/jquery.jqGrid.min.js';
 document.head.appendChild(script2); 
 
+var link = document.createElement("link");
+link.href = "//cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css";
+link.rel = "stylesheet";
+document.head.appendChild(link);
+
 let script3 = document.createElement("script");
-script3.src = 'https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js';
+script3.src = 'https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js';
 document.head.appendChild(script3); 
+
